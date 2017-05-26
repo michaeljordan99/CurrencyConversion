10d8ae9015444ba2902bcad6f2408b180c90ceab
MichaelJordan99 <michaeljordan99@hotmail.com>
2017-05-26 15:33:36 -0700

    refactored into get_rate method

--- app.py
+++ app.py
@@ -22,16 +22,7 @@ def hello_name(name):
 @app.route('/rate/<string:currency1>/to/<string:currency2>')
 def currency_rate(currency1, currency2):
 
-	data = requests.get('http://www.apilayer.net/api/live?access_key=88b5656f48dc23632ce1e4ce7150bd63&currencies=USD,GBP,EUR&format=1').json()
-
-	rate = 0
-
-	if currency2.lower() == 'gbp':
-		rate = data['quotes']['USDGBP']
-	elif currency2.lower() == 'eur':
-		rate = data['quotes']['USDEUR']
-	elif currency2.lower() == 'usd':
-		rate = data['quotes']['USDUSD']
+	rate = get_rate(currency1, currency2)
 
 	return jsonify(
 		{	
@@ -44,13 +35,44 @@ def currency_rate(currency1, currency2):
 @app.route('/convert/<string:currency1>/<float:amount>/to/<string:currency2>')
 def currency_convert(currency1, amount, currency2):
 
-	converted_amount = 23.06
+	rate = get_rate(currency1, currency2)
+
+	converted_amount = round(amount * rate, 2)
 
 	return jsonify(
 		{	
 			'from': '{}'.format(currency1), 
 			'from_amount':'{}'.format(amount),
 			'to': '{}'.format(currency2),
-			'converted_amount': '{}'.format(converted_amount)
+			'converted_amount': '{}'.format(converted_amount),
+			'rate': '{}'.format(rate)
 		})
+
+# Call API to get latest rates - https://currencylayer.com/quickstart 
+def get_rate(currency1, currency2):
+
+	data = requests.get('http://www.apilayer.net/api/live?access_key=88b5656f48dc23632ce1e4ce7150bd63&currencies=USD,GBP,EUR&format=1').json()
+
+	currency1_rate = 0
+
+	if currency1.lower() == 'gbp':
+		currency1_rate = data['quotes']['USDGBP']
+	elif currency1.lower() == 'eur':
+		currency1_rate = data['quotes']['USDEUR']
+	elif currency1.lower() == 'usd':
+		currency1_rate = data['quotes']['USDUSD']
+
+	currency2_rate = 0
+
+	if currency2.lower() == 'gbp':
+		currency2_rate = data['quotes']['USDGBP']
+	elif currency2.lower() == 'eur':
+		currency2_rate = data['quotes']['USDEUR']
+	elif currency2.lower() == 'usd':
+		currency2_rate = data['quotes']['USDUSD']
+
+	if currency1_rate > currency2_rate:
+		return currency2_rate;
+	else:
+		return 1 / currency1_rate;
 
