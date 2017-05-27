b0cad83d10cba249bf35e65611a39fe830ad5bfd
MichaelJordan99 <michaeljordan99@hotmail.com>
2017-05-27 11:36:00 -0700

    added 400 for status_code

--- app.py
+++ app.py
@@ -18,45 +18,55 @@ def hello_name(name):
 # Discogs Marketplace Squad - currency conversion endpoints
 
 # GET /rate/currency1/to/currency2
-@app.route('/rate/<string:currency1>/to/<string:currency2>')
+@app.route('/rate/<string:currency1>/to/<string:currency2>', methods = ['GET'])
 def currency_rate(currency1, currency2):
 
 	if not validation_service.currency_is_valid(currency1) or not validation_service.currency_is_valid(currency2):
-		return jsonify({'error': 'Currency not supported'})
+		response = jsonify({'error': 'Currency not supported'})
+		response.status_code = 400
+		return response
 
 	rate = currency_service.get_rate(currency1, currency2)
 
 	if rate == 0:
 		return jsonify({'error': 'Currency not found'})
 
-	return jsonify(
-		{	
-			'from': '{}'.format(currency1), 
-			'to': '{}'.format(currency2),
-			'rate': '{}'.format(rate)
-		})
+	response = jsonify(
+				{	
+					'from': '{}'.format(currency1), 
+					'to': '{}'.format(currency2),
+					'rate': '{}'.format(rate)
+				})
+
+	return response
 
 # GET /convert/currency1/amount/to/currency2
-@app.route('/convert/<string:currency1>/<float:amount>/to/<string:currency2>')
+@app.route('/convert/<string:currency1>/<float:amount>/to/<string:currency2>', methods = ['GET'])
 def currency_convert(currency1, amount, currency2):
 
 	if not validation_service.amount_is_valid(amount):
-		return jsonify({'error': 'Amount is not valid'})
+		response = jsonify({'error': 'Amount is not valid'})
+		response.status_code = 400
+		return response
 
 	if not validation_service.currency_is_valid(currency1) or not validation_service.currency_is_valid(currency2):
-		return jsonify({'error': 'Currency not supported'})
+		response = jsonify({'error': 'Currency not supported'})
+		response.status_code = 400
+		return response
 
 	rate = currency_service.get_rate(currency1, currency2)
 
 	converted_amount = round(amount * rate, 2)
 
-	return jsonify(
-		{	
-			'from': '{}'.format(currency1), 
-			'from_amount':'{}'.format(amount),
-			'to': '{}'.format(currency2),
-			'converted_amount': '{}'.format(converted_amount),
-			'rate': '{}'.format(rate)
-		})
+	response = jsonify(
+				{	
+					'from': '{}'.format(currency1), 
+					'from_amount':'{}'.format(amount),
+					'to': '{}'.format(currency2),
+					'converted_amount': '{}'.format(converted_amount),
+					'rate': '{}'.format(rate)
+				})
+
+	return response
 
 
